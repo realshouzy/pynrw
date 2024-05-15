c99e472dbe601012559a9158787032dfbcefed6f
shouzy <82171453+realshouzy@users.noreply.github.com>
2024-05-15 23:38:39 +0200

    Futher improve `Connection`

--- nrw/network/_connection.py
+++ nrw/network/_connection.py
@@ -27,6 +27,12 @@ class Connection:
     __slots__: Final[tuple[str, str, str]] = ("_socket", "_to_server", "_from_server")
 
     def __init__(self, server_ip: str, server_port: int) -> None:
+        """Ein Objekt vom Typ `Connection` wird erstellt. Dadurch wird eine Verbindung
+        zum durch `server_ip` und `server_port` spezifizierten Server aufgebaut,
+        so dass Daten (Zeichen-ketten) gesendet und empfangen werden können.
+        Kann die Verbindung nicht hergestellt werden, kann die Instanz von Connection
+        nicht mehr verwendet werden.
+        """
         try:
             self._socket: socket.socket | None = socket.socket(
                 socket.AF_INET,
@@ -41,26 +47,39 @@ class Connection:
                 mode="r",
                 encoding="utf-8",
             )
-        except Exception:  # pylint: disable=W0718
+        except OSError:
             self._socket = None
             self._to_server = None
             self._from_server = None
 
     def receive(self) -> str | None:
+        """Es wird beliebig lange auf eine eingehende Nachricht vom Server gewartet und
+        diese Nachricht anschließend zurückgegeben. Der vom Server angehängte
+        Zeilentrenner wird zuvor entfernt. Während des Wartens ist der ausführende
+        Prozess blockiert. Wurde die Verbindung unterbrochen oder durch den Server
+        unvermittelt geschlossen, wird `None` zurückgegeben.
+        """
         if self._from_server is not None:
-            with suppress(IOError, ValueError):
-                return self._from_server.readline()
+            with suppress(OSError, ValueError):
+                received_line: str = self._from_server.readline().strip()
+                return received_line if received_line else None
         return None
 
     def send(self, message: str) -> None:
+        """Die Nachricht `message` wird - um einen Zeilentrenner ergänzt - an den Server
+        gesendet. Schlägt der Versand fehl, geschieht nichts.
+        """
         if self._to_server is not None:
-            with suppress(ValueError):
+            with suppress(OSError, ValueError):
                 self._to_server.write(f"{message}\n")
                 self._to_server.flush()
 
     def close(self) -> None:
+        """Die Verbindung zum Server wird getrennt und kann nicht mehr verwendet werden.
+        War die Verbindung bereits getrennt, geschieht nichts.
+        """
         if self._socket is not None:
-            with suppress(IOError):
+            with suppress(OSError):
                 self._to_server.close()
                 self._from_server.close()
                 self._socket.shutdown(socket.SHUT_RDWR)
